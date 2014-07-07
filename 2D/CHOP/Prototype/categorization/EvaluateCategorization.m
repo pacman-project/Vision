92e021289f19bcf67622e8eeda590fa4ab1c5209
rusen <rusenaktas@gmail.com>
2014-07-07 11:31:19 +0100

    Missing performance evaluation code.

---
+++ 2D/CHOP/Prototype/categorization/EvaluateCategorization.m
@@ -0,0 +1,31 @@
+%> Name: EvaluateCategorization
+%>
+%> Description: Evaluates the categorization performance of the dataset.
+%>
+%> @param datasetName The name of the dataset.
+%> 
+%> Author: Rusen
+%>
+%> Updates
+%> Ver 1.0 on 04.07.2014
+function [ ] = EvaluateCategorization( datasetName )
+    options = SetParameters(datasetName, 'train');
+    outputFolder = options.testInferenceFolder;
+    fileNames = fuf([outputFolder '/*.mat'], 1, 'detail');
+    gtArr = zeros(size(fileNames,1), 1);
+    detectionArr = zeros(size(fileNames,1), 1);
+    for fileItr = 1:numel(fileNames)
+       load(fileNames{fileItr}, 'categoryLabel', 'estimatedCategoryLabel');
+       gtArr(fileItr) = categoryLabel;
+       detectionArr(fileItr) = estimatedCategoryLabel;
+    end
+    
+    % Estimate performance, and find confusion matrix.
+    accuracy = numel(find(detectionArr == gtArr)) / numel(fileNames);
+    confMat = confusionmat(gtArr, detectionArr);
+    save([options.outputFolder '/perf.mat'], 'accuracy', 'confMat');
+    display(['Accuracy: ' num2str(accuracy)]);
+    display('Confusion matrix:');
+    display(mat2str(confMat));
+end
+
