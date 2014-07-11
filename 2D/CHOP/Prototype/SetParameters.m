cdacc7e3827746b03a0a46f05f50869610f0f1a5
rusen <rusenaktas@gmail.com>
2014-07-11 15:21:09 +0100

    Parameter change for export.

--- 2D/CHOP/Prototype/SetParameters.m
+++ 2D/CHOP/Prototype/SetParameters.m
@@ -209,7 +209,7 @@ function [ options ] = SetParameters( datasetName, isTraining )
                                 % format is changed. They are not
                                 % parameters, and should not be changed
                                 % unless SUBDUE output format is changed.
-    options.subdue.threshold = 0.03; % Theshold for elasticity-based matching 
+    options.subdue.threshold = 0.05; % Theshold for elasticity-based matching 
                                     % in SUBDUE. Can be in [0,1]. 0: Strict
                                     % matching, (-> 1) Matching gets looser.
     options.subdue.minSize = 2; % Minimum number of nodes in a composition 
