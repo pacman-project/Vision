76d6882bbdfece390421d3a5331f42779294d4f2
rusen <rusenaktas@gmail.com>
2014-07-11 09:15:33 +0100

    Category score normalization in MarkCategoryLabels.m

--- 2D/CHOP/Prototype/categorization/MarkCategoryLabels.m
+++ 2D/CHOP/Prototype/categorization/MarkCategoryLabels.m
@@ -38,10 +38,19 @@ function [ ] = MarkCategoryLabels( datasetName )
             realArr{partItr} = categoryArrIdx(realizations(realizations(:,1) == partItr, 5));
         end
         
+        % Find normalization constants for each category.
+        realCategoryArr = categoryArrIdx(realizations(:, 5));
+        normConstants = hist(realCategoryArr, 1:numberOfCategories);
+        normConstants = normConstants/sum(normConstants);
+        normConstants = 1./normConstants;
+        
         % Process each node in the vocabulary.
         probArr = cell(numberOfParts,1);
         for partItr = 1:numel(vocabLevel)
-            probArr{partItr} = hist(realArr{partItr}, 1:numberOfCategories) / numel(realArr{partItr});
+            assgnArr = (hist(realArr{partItr}, 1:numberOfCategories) / numel(realArr{partItr}));
+            assgnArr = assgnArr .* normConstants;
+            assgnArr = assgnArr / sum(assgnArr);
+            probArr{partItr} = assgnArr;
         end
         
         % Save probabilities.
