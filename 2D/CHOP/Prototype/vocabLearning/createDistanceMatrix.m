efec182f6bacabf06e607c4544355a38a97eb6a7
Rusen <rusenaktas@gmail.com>
2014-10-29 15:03:48 +0000

    Changing distance matrix creation so that filters are compared on their center of gravity.

--- 2D/CHOP/Prototype/vocabLearning/createDistanceMatrix.m
+++ 2D/CHOP/Prototype/vocabLearning/createDistanceMatrix.m
@@ -6,26 +6,46 @@
 %>
 %> @param options Program options.
 %>
-%> @retval simMat Distance matrix of level 1 parts.
+%> @retval distMat Distance matrix of level 1 parts.
 %> 
 %> Author: Rusen
 %>
 %> Updates
 %> Ver 1.0 on 01.09.2014
 function [ distMat ] = createDistanceMatrix( filters )
-    % Initialize internal data structures.
+    cogFilters = cell(numel(filters),1);
     numberOfFilters = numel(filters);
     distMat = zeros(numel(filters));
-
-    % Find similarity between each pair of filters.
-    for filtItr = 1:(numberOfFilters-1)
+    filterSize = [size(filters{1},1), size(filters{1},2)];
+    binaryMask = true(filterSize);
+    cog = zeros(1,2);
+    trueCenter = 1 + (filterSize-1)/2;
+    % Find center of gravity for each filter, and center it around its
+    % cog to estimate correct distance between pairs of filters.
+    for filtItr = 1:numel(filters)
         filter1 = filters{filtItr};
+        filter1(filter1<0) = filter1(filter1<0) * -1;
+        filter1Gray = mean(filter1/ max(max(max(filter1))),3);
+        % Assign small things to zero.
+        minVal = max(max(filter1Gray)) * 0.2;
+        filter1Gray(filter1Gray < max(max(filter1Gray)) * 0.2) = 0;
+        measurements = regionprops(binaryMask, filter1Gray, 'WeightedCentroid');
+        cog([2 1]) = measurements(1).WeightedCentroid;
+        cogDiff = round(trueCenter - cog);
+        newFilter = circshift(filters{filtItr}, cogDiff);
+        newFilter(newFilter < minVal & newFilter > -minVal) = 0;
+        cogFilters(filtItr) = {newFilter};
+    end
+    % Find distance between each pair of filters (cog-normalized).
+    for filtItr = 1:(numberOfFilters-1)
+        filter1 = cogFilters{filtItr};
         for filtItr2 = (filtItr+1):numberOfFilters
-            filter2 = filters{filtItr2};
+            filter2 = cogFilters{filtItr2};
             distance = sqrt(sum(sum(sum((filter1-filter2).^2))));
             distMat(filtItr, filtItr2) = distance;
             distMat(filtItr2, filtItr) = distance;
         end
     end
+    % Normalize distMat.
     distMat = distMat/max(max(distMat));
 end
