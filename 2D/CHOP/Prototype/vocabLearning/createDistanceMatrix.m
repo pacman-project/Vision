67cf7a77132adaf4e54d4eadb8ebc866abb9cb31
rusen <rusenaktas@gmail.com>
2014-10-29 16:49:35 +0000

    Distance function changed for level 1 filters. The default distance function in manopt (manifold optimization toolbox) used.

--- 2D/CHOP/Prototype/vocabLearning/createDistanceMatrix.m
+++ 2D/CHOP/Prototype/vocabLearning/createDistanceMatrix.m
@@ -41,11 +41,27 @@ function [ distMat ] = createDistanceMatrix( filters )
         filter1 = cogFilters{filtItr};
         for filtItr2 = (filtItr+1):numberOfFilters
             filter2 = cogFilters{filtItr2};
-            distance = sqrt(sum(sum(sum((filter1-filter2).^2))));
+            distance = findDistance(filter1, filter2);
             distMat(filtItr, filtItr2) = distance;
             distMat(filtItr2, filtItr) = distance;
         end
     end
     % Normalize distMat.
     distMat = distMat/max(max(distMat));
+end
+
+function distance = findDistance(filter1, filter2)
+    square_d = 0;
+    XtY = multiprod(multitransp(filter1), filter2);
+    for i = 1 : size(filter1,3)
+         cos_princ_angle = svd(XtY(:, :, i));
+         % Two next instructions not necessary: the imaginary parts that
+         % would appear if the cosines are not between -1 and 1 when
+         % passed to the acos function would be very small, and would
+         % thus vanish when the norm is taken.
+         % cos_princ_angle = min(cos_princ_angle,  1);
+         % cos_princ_angle = max(cos_princ_angle, -1);
+         square_d = square_d + norm(acos(cos_princ_angle))^2;
+    end
+     distance = sqrt(square_d);
 end
