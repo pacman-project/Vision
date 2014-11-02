7b24d7d8a548e1e04036fc285f9f469c10af0a97
Rusen <rusenaktas@gmail.com>
2014-11-02 15:08:51 +0000

    Parameter changes in STL-10 dataset.

--- 2D/CHOP/Prototype/parameters/SetParametersSTL10.m
+++ 2D/CHOP/Prototype/parameters/SetParametersSTL10.m
@@ -49,8 +49,8 @@ function [ options ] = SetParametersSTL10( datasetName, options )
                                         % be surpressed.
     options.autoFilterSize = 10;         % Size (one side) of a autodetected 
                                         % filter. Assumed to be NxNxD.
-    options.auto.inhibitionRadius = floor(options.autoFilterSize/2)-1;
-    options.autoFilterThr = 0.25;       % Min response threshold for convolved 
+    options.auto.inhibitionRadius = floor(options.autoFilterSize/2)+1;
+    options.autoFilterThr = 0.2;       % Min response threshold for convolved 
                                        % features, assigned as this percentage 
                                        % of the max response in each image.
     options.autoFilterCount = 200;      % Number of auto-detected filters.
@@ -61,7 +61,7 @@ function [ options ] = SetParametersSTL10( datasetName, options )
                                        % auto-filter mode, since gabors are
                                        % extracted using conv2, convolution
                                        % implementation of matlab.
-    options.auto.deadFeatureStd = 0.05; % In case of auto-learned features, 
+    options.auto.deadFeatureStd = 0.00; % In case of auto-learned features, 
                                        % some dead features may come up.
                                        % The standard deviation check is
                                        % used to eliminate uniform
@@ -216,13 +216,13 @@ function [ options ] = SetParametersSTL10( datasetName, options )
                                            % edgeLabelId (int, 4 byte) + 
                                            % destinationNode (int,4 byte) + 
                                            % isDirected (byte, 1 byte) = 9.
-    options.subdue.maxTime = 1200;          % Max. number of seconds subdue is
+    options.subdue.maxTime = 1800;          % Max. number of seconds subdue is
                                             % allowed to run. Typically
                                             % around 100 (secs) for toy data. 
                                             % You can set to higher values
                                             % (e.g. 3600 secs) for large
                                             % datasets.
-    options.subdue.threshold = 0.04; % Theshold for elastic part matching. 
+    options.subdue.threshold = 0.025; % Theshold for elastic part matching. 
                                     % Can be in [0,1]. 
                                     % 0: Strict matching, 
                                     % (value -> 1) Matching criterion 
@@ -234,7 +234,7 @@ function [ options ] = SetParametersSTL10( datasetName, options )
                                     % parts.
     options.subdue.minSize = 2; % Minimum number of nodes in a composition.
     options.subdue.maxSize = 3; % Maximum number of nodes in a composition.
-    options.subdue.nsubs = 10000;  % Maximum number of nodes allowed in a level.
+    options.subdue.nsubs = 20000;  % Maximum number of nodes allowed in a level.
     options.subdue.beam = 200;   % Beam length in SUBDUE' search mechanism.
     options.subdue.overlap = false;   % If true, overlaps between a substructure's 
                                      % instances are considered in the
@@ -245,7 +245,7 @@ function [ options ] = SetParametersSTL10( datasetName, options )
                                      % returned anyway in order to
                                      % introduce redundancy in the final
                                      % object graphs.
-     options.subdue.supervised = false; % If true, graph search is performed over
+     options.subdue.supervised = true; % If true, graph search is performed over
 				          % the whole data. If not, individual categories 
 			                  % are searched, and the vocabularies are then 
 			                  % combined.
