ee516ac22e916ec7412b7c7ff98f9e4d77f16661
Rusen <rusenaktas@gmail.com>
2014-11-03 18:36:57 +0000

    Added a the original image patches as shadow images to the printed neighbor image.

--- 2D/CHOP/Prototype/utilities/printCloseFilters.m
+++ 2D/CHOP/Prototype/utilities/printCloseFilters.m
@@ -8,6 +8,20 @@ function [finalImg] = printCloseFilters(distanceMatrix, levelItr, options)
             filters{filterItr} = imread([options.currentFolder '/debug/' datasetName '/level' num2str(levelItr) '/reconstruction/' num2str(filterItr) '_uni.png']);
         end
     end
+    for filterItr = 1:numel(filters)
+       filePath = [options.currentFolder '/debug/' options.datasetName '/level' num2str(levelItr) '/cropped/' num2str(filterItr) '_1.png'];
+       if exist(filePath, 'file')
+           orgImg = imread(filePath);
+       else
+           orgImg = [];
+       end
+       filter1 = double(filters{filterItr});
+       filter1 = uint8(255 * (filter1 - min(min(min(filter1)))) / (max(max(max(filter1))) - min(min(min(filter1)))));
+       if ~isempty(orgImg)
+           filter1 = uint8(round(double(filter1) * 0.7 + double(orgImg) * 0.3));
+       end
+       filters{filterItr} = filter1;
+    end
     numberOfFilters = numel(filters);
     shownFilters = min(size(distanceMatrix,1), 6);
     imgDim = size(filters{1});
@@ -20,8 +34,6 @@ function [finalImg] = printCloseFilters(distanceMatrix, levelItr, options)
             startX = 2 + (filterItr-1) * (imgDim(1)+1);
             startY = 2 + (shownItr-1) * (imgDim(1)+1);
             filter1 = filters{rankings(shownItr)};
-            filter1 = double(filter1);
-            filter1 = uint8(255 * (filter1 - min(min(min(filter1)))) / (max(max(max(filter1))) - min(min(min(filter1)))));
             finalImg(startX:(startX+imgDim(1)-1), startY:(startY+imgDim(2)-1), :) = filter1;
         end
     end
