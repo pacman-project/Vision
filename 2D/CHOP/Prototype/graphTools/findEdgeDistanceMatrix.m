9b60a5cf05fdc4c82ccecc45358cc11d2a6933fc
Rusen <rusenaktas@gmail.com>
2014-11-06 00:27:15 +0000

    Missing file.

---
+++ 2D/CHOP/Prototype/graphTools/findEdgeDistanceMatrix.m
@@ -0,0 +1,23 @@
+function [ edgeIdMatrix, edgeDistanceMatrix, edgeCoords ] = findEdgeDistanceMatrix( edgeQuantize )
+    % Find a round area in which we will look for individual relations.
+    edgeIdMatrix = zeros(edgeQuantize, 'int32');
+    centerPointSingle = (edgeQuantize+1)/2; 
+    yArr = repmat(1:edgeQuantize, edgeQuantize, 1);
+    yArrAbs = abs(yArr - centerPointSingle);
+    xArr = repmat((1:edgeQuantize)', 1, edgeQuantize);
+    xArrAbs = abs(xArr - centerPointSingle);
+    distances = sqrt(xArrAbs .^2 + yArrAbs .^2);
+    validPointMatrix = distances<=floor(centerPointSingle);
+    
+    % Identify each point in the area as an individual relation.
+    relations = find(validPointMatrix);
+    [coordX, coordY] = ind2sub(size(validPointMatrix), relations);
+    edgeCoords = [coordX, coordY] - floor(centerPointSingle);
+    edgeIdMatrix(relations) = 1:numel(relations);
+
+    % Find the distance matrix with respect to pairs of individual
+    % relations.
+    points = [xArr(relations), yArr(relations)];
+    edgeDistanceMatrix = squareform(pdist(points)) / edgeQuantize;
+end
+
